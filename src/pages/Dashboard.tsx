29ce6531b1b0305a4578e75f8e8e67dccfc3e805
ssm825 <ssm39825@gmail.com>
2022-10-18 19:21:19 +0900

    ADD : form layout

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Table from 'components/Table';
 import SideBar from 'components/SideBar';
+import { PageSummary } from 'components/PageSummary';
 
 const Dashboard = () => {
   return (
     <main className="flex w-full">
       <SideBar />
-      <Table />
+      {/* <Table /> */}
+      <PageSummary />
     </main>
   );
 };
