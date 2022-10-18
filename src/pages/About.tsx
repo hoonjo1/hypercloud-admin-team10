f5f2c82e8390f66ccfb1446c79973f170d021397
zzangzzong92 <zzangzzong92@gmail.com>
2022-10-18 19:25:21 +0900

    ADD: create About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export const BusinessArea = () => {
   return (
     <div className="p-5 w-4/5 overflow-x-auto relative shadow-md sm:rounded-lg">
@@ -82,7 +84,7 @@ export const BusinessArea = () => {
             type="text"
             id="default-input"
             disabled
-            placeholder="XR Business"
+            placeholder="About"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
         </div>
