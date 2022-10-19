2f875daeea6245eff84eb3187c3c2ba2ca0b16c6
ssm825 <ssm39825@gmail.com>
2022-10-19 19:20:28 +0900

    ADD : XR-Business data fetch - business data fetch - modal 수정

---
+++ src/components/Modal.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+export const Modal = () => {
+  return (
+    <div
+      id="defaultModal"
+      tabIndex={-1}
+      aria-hidden="true"
+      className="flex justify-center items-center bg-gray-900/50 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full"
+    >
+      <div className="relative right-0 p-4 w-full max-w-2xl h-full md:h-auto">
+        <div className="relative top-50% left-50% bg-white rounded-lg shadow dark:bg-gray-700">
+          <div className="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
+            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+              Terms of Service
+            </h3>
+            <button
+              type="button"
+              // onClick={handleModal}
+              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              data-modal-toggle="defaultModal"
+            >
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              <span className="sr-only">Close modal</span>
+            </button>
+          </div>
+
+          <div className="p-6 space-y-6">
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              With less than a month to go before the European Union enacts new
+              consumer privacy laws for its citizens, companies around the world
+              are updating their terms of service agreements to comply.
+            </p>
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              The European Union’s General Data Protection Regulation (G.D.P.R.)
+              goes into effect on May 25 and is meant to ensure a common set of
+              data rights in the European Union. It requires organizations to
+              notify users as soon as possible of high-risk data breaches that
+              could personally affect them.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
